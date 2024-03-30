70d32c94e14dbb07d17978e0fde66ac155ea8d5b
Christian <christianmpacheco@gmail.com>
2024-03-30 12:54:20 -0700

    problem 4

--- problems/04-remove-e-words-filter.js
+++ problems/04-remove-e-words-filter.js
@@ -13,8 +13,19 @@ console.log(removeEWords('Enter the building')); // 'building'
 */
 
 let removeEWords = function(sentence) {
-    // Your code here 
+    let sent = '';
+    let arr = sentence.split(' ');
+    const result = arr.filter((e) => {
+      if(!e.includes('e')) {
+        sent += e + ' ';
+      }
+    });
+    return sent.trim();
 };
+
+console.log(removeEWords('What time is it everyone?')); // 'What is it'
+console.log(removeEWords('Enter the building')); // 'building'
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
