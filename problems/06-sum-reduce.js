87056cd8546cd3148fd1eab051cb8575f04b8f9c
Christian <christianmpacheco@gmail.com>
2024-03-30 14:14:38 -0700

    problem 6

--- problems/06-sum-reduce.js
+++ problems/06-sum-reduce.js
@@ -13,8 +13,9 @@ console.log(sumWithReduce([])); // 0
 */
 
 let sumWithReduce = function(nums) {
-  // Your code here 
+  return nums.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 };
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
